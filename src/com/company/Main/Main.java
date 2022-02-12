6b60e427e01239185651fe7dfa02cf7b04fbf00e
Djailson1 <djailson_silva1@outlook.com>
2022-02-11 22:48:23 -0300

    Exercício de variáveis

---
+++ src/com/company/Main/Main.java
@@ -0,0 +1,63 @@
+package com.company.Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2786748007863476667L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.840;
+
+        char c1 = 'H';
+        //char c2 = 'Tn';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "hgfdahgdshghg652365dsjhhsd";
+
+        String dt1 = "09/02/1991";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+
+    }
+}
